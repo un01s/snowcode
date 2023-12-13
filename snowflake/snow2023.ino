610ba44863056ab19ae40a89e7993748544a5e12
un01s <96022876+un01s@users.noreply.github.com>
2023-12-13 10:14:54 -0700

    update

--- snowflake/snow2023.ino
+++ snowflake/snow2023.ino
@@ -86,12 +86,16 @@ void setup() {
 uint8_t gCurrentPatternNumber = 0;
 uint8_t InitNeeded = 1;
 
+static uint8_t colorMotion = 0;
+
 #define ARRAY_SIZE(A) (sizeof(A)/sizeof((A)[0]))
 typedef void (*PatternList[])();
 PatternList gPatterns = {
+  pacifica_loop,
   rainbowbeat,
   twinkle,
-  pride  
+  pride,
+  breathe  
 };
 
 void loop() {
@@ -107,15 +111,19 @@ void loop() {
   //pride();
   //FastLED.show();
   
-  EVERY_N_SECONDS(15) {
-    //fadeOut(50);
-    gCurrentPatternNumber = (gCurrentPatternNumber+1)%ARRAY_SIZE(gPatterns);
-    //InitNeeded = 1;
-    //fadeIn(50);  
+  ChangePalettePeriodically();
+  uint8_t maxChanges = 24; 
+  nblendPaletteTowardPalette(currentPalette, targetPalette, maxChanges);
+  
+  EVERY_N_SECONDS(60) {
+    gCurrentPatternNumber = (gCurrentPatternNumber+1)%ARRAY_SIZE(gPatterns);  
   }
 
   gPatterns[gCurrentPatternNumber]();
 
+  if (gCurrentPatternNumber == 0) {
+    colorMotion += 2; // steps to move     
+  }
   FastLED.show();
 }
 
@@ -127,7 +135,7 @@ void test_loop() {
   
   static uint8_t startIndex = 0;
   //twinkle();
-  FillRingsFromPaletteColors(startIndex);
+  //FillRingsFromPaletteColors(startIndex);
 
   startIndex = startIndex + 2; /* motion speed */
   
@@ -137,8 +145,13 @@ void test_loop() {
 } // loop()
 
 // the direction is from the center to outlines
-void FillRingsFromPaletteColors( uint8_t colorIndex)
+//void FillRingsFromPaletteColors( uint8_t colorIndex)
+void ringout()
 {
+    uint8_t steps = (millis()/500)%2;
+    
+    if (steps == 1) {
+    uint8_t colorIndex = colorMotion;
     // LINEARBLEND is better than NOBLEND, it makes the animation smoother.
     uint8_t i, j, index;
     CRGB c1 = ColorFromPalette( currentPalette, colorIndex, MAX_BRIGHTNESS, LINEARBLEND);
@@ -178,6 +191,7 @@ void FillRingsFromPaletteColors( uint8_t colorIndex)
       }
       colorIndex += 2; // 4, small steps better?
     }
+    }
 }
 
 void ChangePalettePeriodically()
@@ -210,6 +224,28 @@ void ChangePalettePeriodically()
   }
 }
 
+void SetupTotallyRandomPalette()
+{
+    for( int i = 0; i < 16; ++i) {
+        currentPalette[i] = CHSV( random8(), 255, random8());
+    }
+}
+
+void SetupMyPalette()
+{
+    CRGB r = CHSV( HUE_RED, 255, 255);
+    CRGB p = CHSV( HUE_PURPLE, 255, 255);
+    CRGB y = CHSV( HUE_YELLOW, 255, 255);
+    CRGB o = CHSV( HUE_ORANGE, 255, 255);
+    CRGB g = CHSV( HUE_GREEN, 255, 255);
+    CRGB b = CRGB::Blue;
+    
+    currentPalette = CRGBPalette16(g, g, r,  r,
+                                   y, y, b,  b,
+                                   p, p, r,  b,
+                                   b, r, b,  y);
+}
+
 void twinkle() {
 
   random16_set_seed(535);                                                           // The randomizer needs to be re-set each time through the loop in order for the 'random' numbers to be the same each time through.
@@ -224,7 +260,8 @@ void twinkle() {
 } // twinkle()
 
 // breathing with changing color
-void breathe(uint8_t start) {
+void breathe() {
+  uint8_t start = random8();
   uint8_t brightness = (exp(sin(millis() / 2000.0 * PI)) - 0.368) * 42.546;
   
   FastLED.setBrightness(brightness);
@@ -241,8 +278,9 @@ void breathe(uint8_t start) {
   if (brightness > 90 && flag == 0) {
     flag = 1;  
   }
-  
-  FastLED.show();      
+
+  // used as one pattern
+  //FastLED.show();      
 }
 
 // this is too fast
@@ -316,4 +354,96 @@ void fadeOut(byte steps) {
     FastLED.setBrightness(fadeOut);
     FastLED.show();
   }  
+}
+
+// pacifica
+CRGBPalette16 pacifica_palette_1 = 
+    { 0x000507, 0x000409, 0x00030B, 0x00030D, 0x000210, 0x000212, 0x000114, 0x000117, 
+      0x000019, 0x00001C, 0x000026, 0x000031, 0x00003B, 0x000046, 0x14554B, 0x28AA50 };
+CRGBPalette16 pacifica_palette_2 = 
+    { 0x000507, 0x000409, 0x00030B, 0x00030D, 0x000210, 0x000212, 0x000114, 0x000117, 
+      0x000019, 0x00001C, 0x000026, 0x000031, 0x00003B, 0x000046, 0x0C5F52, 0x19BE5F };
+CRGBPalette16 pacifica_palette_3 = 
+    { 0x000208, 0x00030E, 0x000514, 0x00061A, 0x000820, 0x000927, 0x000B2D, 0x000C33, 
+      0x000E39, 0x001040, 0x001450, 0x001860, 0x001C70, 0x002080, 0x1040BF, 0x2060FF };
+
+void pacifica_loop()
+{
+  // Increment the four "color index start" counters, one for each wave layer.
+  // Each is incremented at a different speed, and the speeds vary over time.
+  static uint16_t sCIStart1, sCIStart2, sCIStart3, sCIStart4;
+  static uint32_t sLastms = 0;
+  uint32_t ms = GET_MILLIS();
+  uint32_t deltams = ms - sLastms;
+  sLastms = ms;
+  uint16_t speedfactor1 = beatsin16(3, 179, 269);
+  uint16_t speedfactor2 = beatsin16(4, 179, 269);
+  uint32_t deltams1 = (deltams * speedfactor1) / 256;
+  uint32_t deltams2 = (deltams * speedfactor2) / 256;
+  uint32_t deltams21 = (deltams1 + deltams2) / 2;
+  sCIStart1 += (deltams1 * beatsin88(1011,10,13));
+  sCIStart2 -= (deltams21 * beatsin88(777,8,11));
+  sCIStart3 -= (deltams1 * beatsin88(501,5,7));
+  sCIStart4 -= (deltams2 * beatsin88(257,4,6));
+
+  // Clear out the LED array to a dim background blue-green
+  fill_solid( leds, NUM_LEDS, CRGB( 2, 6, 10));
+
+  // Render each of four layers, with different scales and speeds, that vary over time
+  pacifica_one_layer( pacifica_palette_1, sCIStart1, beatsin16( 3, 11 * 256, 14 * 256), beatsin8( 10, 70, 130), 0-beat16( 301) );
+  pacifica_one_layer( pacifica_palette_2, sCIStart2, beatsin16( 4,  6 * 256,  9 * 256), beatsin8( 17, 40,  80), beat16( 401) );
+  pacifica_one_layer( pacifica_palette_3, sCIStart3, 6 * 256, beatsin8( 9, 10,38), 0-beat16(503));
+  pacifica_one_layer( pacifica_palette_3, sCIStart4, 5 * 256, beatsin8( 8, 10,28), beat16(601));
+
+  // Add brighter 'whitecaps' where the waves lines up more
+  pacifica_add_whitecaps();
+
+  // Deepen the blues and greens a bit
+  pacifica_deepen_colors();
+}
+
+// Add one layer of waves into the led array
+void pacifica_one_layer( CRGBPalette16& p, uint16_t cistart, uint16_t wavescale, uint8_t bri, uint16_t ioff)
+{
+  uint16_t ci = cistart;
+  uint16_t waveangle = ioff;
+  uint16_t wavescale_half = (wavescale / 2) + 20;
+  for( uint16_t i = 0; i < NUM_LEDS; i++) {
+    waveangle += 250;
+    uint16_t s16 = sin16( waveangle ) + 32768;
+    uint16_t cs = scale16( s16 , wavescale_half ) + wavescale_half;
+    ci += cs;
+    uint16_t sindex16 = sin16( ci) + 32768;
+    uint8_t sindex8 = scale16( sindex16, 240);
+    CRGB c = ColorFromPalette( p, sindex8, bri, LINEARBLEND);
+    leds[i] += c;
+  }
+}
+
+// Add extra 'white' to areas where the four layers of light have lined up brightly
+void pacifica_add_whitecaps()
+{
+  uint8_t basethreshold = beatsin8( 9, 55, 65);
+  uint8_t wave = beat8( 7 );
+  
+  for( uint16_t i = 0; i < NUM_LEDS; i++) {
+    uint8_t threshold = scale8( sin8( wave), 20) + basethreshold;
+    wave += 7;
+    uint8_t l = leds[i].getAverageLight();
+    if( l > threshold) {
+      uint8_t overage = l - threshold;
+      uint8_t overage2 = qadd8( overage, overage);
+      leds[i] += CRGB( overage, overage2, qadd8( overage2, overage2));
+    }
+  }
+}
+
+// Deepen the blues and greens
+void pacifica_deepen_colors()
+{
+  for( uint16_t i = 0; i < NUM_LEDS; i++) {
+    leds[i].blue = scale8( leds[i].blue,  145); 
+    leds[i].green= scale8( leds[i].green, 200); 
+    leds[i] |= CRGB( 2, 5, 7);
+  }
 }
